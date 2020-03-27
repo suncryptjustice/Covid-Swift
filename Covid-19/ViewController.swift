d156797d9e42167666b375c5c62c190ec6738b07
Suncrypt <suncrypt@gmail.com>
2020-03-27 04:18:50 +0300

    Initial Commit

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Ilia Zhegunov on 27.03.2020.
+//  Copyright © 2020 Ilia Zhegunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
